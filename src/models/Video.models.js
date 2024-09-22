26b97bf1051ab31cd38e787d1a62f59a420b5547
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-22 13:36:32 +0530

    Video model complete

---
+++ src/models/Video.models.js
@@ -0,0 +1,42 @@
+// VIDEO MODELS
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
