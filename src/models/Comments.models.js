694294c8bf2edffbc764e3bc7936c4f69aecf8a8
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-22 13:51:36 +0530

    comment model done

---
+++ src/models/Comments.models.js
@@ -0,0 +1,21 @@
+// COMMENT MODELS
+import mongoose, { Schema } from "mongoose";
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+export const Comment = mongoose.model("Comment", commentSchema);
