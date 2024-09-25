befba6a9b95f31ccec24f3e65fcb075fec0a21e2
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-25 21:55:48 +0530

    global Error catch defined

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import e from "express";
 const app = e();
 
-
+app.use((err, req, res, next) => {
+  if (err instanceof APIError) {
+    return res.status(err.statusCode || 500).json({
+      success: err.success,
+      message: err.message,
+      errors: err.errors,
+      stack: process.env.MODE_ENV === "development" ? err.stack : undefined,
+    });
+  } else {
+    return res.status(500).json({
+      message: "internal server issue!",
+      success: false,
+    });
+  }
+});
 export default app;
